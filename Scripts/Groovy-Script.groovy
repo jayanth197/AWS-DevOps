2f55cd5fe370d0c6c26309c94515276e7a70197f
jayanth197 <jayanthvarmachennu@gmail.com>
2025-01-15 23:14:50 +0530

    15-01-2025

---
+++ Scripts/Groovy-Script.groovy
@@ -0,0 +1,77 @@
+Git
+Build
+Testing
+Artifactory
+Deployment
+
+
+pipeline {
+    agent any
+    stages {
+        stage('Git') {
+            steps {
+                git url: https://github.com/repo-url
+            }
+        }
+        stage('build') {
+            steps {
+                sh 'maven clean install package'
+            }
+        }
+        stage('Testing') {
+            steps {
+                Junit 'test.xml'
+            }
+        }
+    }
+}
+
+
+Publish Over SSh Using Groovy Script
+
+
+pipeline {
+    agent any
+    stages {
+        stage('scm') {
+            steps {
+                git url: 'https://github.com/jayanth197/WebServerproject_B2.git',
+                    branch: 'main',
+                    credentialsId: 'e23c1943-bed0-428c-92c2-cc10dd5a9bc2'
+            }
+        }
+        stage('ArchiveArtifacts') {
+            steps {
+                archiveArtifacts '**/*.html'
+            }
+        }
+        stage('Build') {
+            steps {
+                sshPublisher(publishers: [
+                    sshPublisherDesc(
+                        configName: 'WebServer_B1',
+                        transfers: [
+                            sshTransfers(
+                                excludes: '',
+                                exeCommand: '',
+                                exeTimeout: 120000,
+                                flatten: true,
+                                makeEmptyDirs: false,
+                                noDefaultExcludes: false,
+                                patternSeparator: '[,]+',
+                                remoteDirectory: 'www/html',
+                                remoteDirectorySDF: false,
+                                removePrefix: '',
+                                sourceFiles: '**/*.html'
+                            )
+                        ],
+                        usePromotionTimestamp: false,
+                        useWorkspaceInPromotion: false,
+                        verbose: true
+                    )
+                ])
+            }
+        }
+
+    }
+}
